c0e2b237cdb4da6a3e2159b0395b0fe7e9f6bc9c
eliza0804 <elizabethh2412@gmail.com>
2025-05-22 17:08:24 -0300

    aprendendo prompt

---
+++ logicacomjs.js
@@ -0,0 +1,4 @@
+let salário =Number (prompt("Digite o salário"))
+let mesestrabalhados =Number (prompt("Digite os meses trabalhados"))
+let decimo = salario/12*mesestrabalhados
+console.log("Seu decimo é:R$" = decimo)
