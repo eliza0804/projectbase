0e887c76e8fd29a76a4c030f0f05353c0bef5832
eliza0804 <elizabethh2412@gmail.com>
2025-06-12 17:01:55 -0300

    inicializando dom

---
+++ index.js
@@ -0,0 +1,9 @@
+/*let leitura = document.getElementsByTagName("li")[3]
+leitura.style.color = "gray" 
+
+let leitura = document.getElementsByName("h4dadiv")[0]
+leitura.innerText = "Já fui e voltei!!"
+leitura.style.color = "white" 
+
+let leitura = document.getElementById("atalaia")
+leitura.style.background = "grey" */
